7d9e4bd282985b4c0ea1f99d258a7b6df0a8dbc2
Slemhrzn <salinamaharjannn@gmail.com>
2024-11-19 21:22:44 +0545

    hompage half done

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -3,14 +3,44 @@ import "./Homepage.css";
 
 const Homepage = () => {
   return (
-    <div className="homepage">
-      <div className="homepage-container">
-        <div className="left-image">
-          <img src="/left-image.jpg" alt="Left" />
+    <div className="homepage_container">
+      <div className="homepage_top">
+        <div className="left">
+          <img src="left-image.jpg" />
         </div>
         <div className="right">
-          <p>Where Technology Meets Vision:</p>
+          <p style={{ fontSize: "25px" }}>Where Technology Meets Vision</p>
+          <p
+            style={{ fontWeight: "lighter", color: "black", fontSize: "18px" }}
+          >
+            Let Us Help You Build, Secure and Accelerate Your Digital
+            Transformation Journey. Unlocking Innovation, Enhancing Agility, and
+            Empowering Growth for a Future-Ready Business.
+          </p>
+          <div className="explore-button"
+          >
+            <button
+              style={{
+                padding: "10px",
+                borderRadius: "20px",
+                border: "none",
+                fontFamily: "var(--mainfont)",
+                fontWeight:"bold",
+                fontSize:"15px",
+                backgroundColor:"#6c63ff",
+                color:"white"
+              }}
+            >
+              Explore about us
+            </button>
+          </div>
         </div>
+        
+
+      </div>
+
+      <div className="homepage_mid">
+        
       </div>
     </div>
   );
